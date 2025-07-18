c6e374e33a9640c0209f138e9d6673643ef83289
CeaslessVoid <hpha0051@student.monash.edu>
2025-07-19 00:04:19 +1000

    123123

---
+++ Source/NeavaMods/NeavaMods/CompWeaponContainer.cs
@@ -0,0 +1,30 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using static NeavaMods.CompProperties_WeaponModContainer;
+
+namespace NeavaMods
+{
+    public class CompWeaponContainer : CompThingContainer
+    {
+        public override bool Accepts(Thing thing)
+        {
+            return !(thing is Building) && thing.TryGetComp<CompWeaponModContainer>() != null;
+        }
+
+        public override bool Accepts(ThingDef thingDef)
+        {
+            return !Empty && thingDef.HasComp<CompWeaponModContainer>();
+        }
+
+        //public override string CompInspectStringExtra()
+        //{
+        //    TaggedString label = base.Empty ? "Nothing".Translate() : base.ContainedThing.LabelCap;
+        //    return ("Contents".Translate() + ": " + label).Trim();
+        //}
+    }
+}
